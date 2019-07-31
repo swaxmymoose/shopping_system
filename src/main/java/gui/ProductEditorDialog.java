566b6a32754a8a63721b6cbaf2fa6eb379b81310
rofth173 <rofth173@SOB0484L.staff.uod.otago.ac.nz>
2019-07-31 18:03:09 +1200

    Fixed typo for combo box name.

--- src/main/java/gui/ProductEditorDialog.java
+++ src/main/java/gui/ProductEditorDialog.java
@@ -28,9 +28,9 @@ public class ProductEditorDialog extends javax.swing.JDialog {
     public ProductEditorDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        cbProductCategory.setEditable(true);
+        cmbProductCategory.setEditable(true);
         myModel.updateItems(collection);
-        cbProductCategory.setModel(myModel);
+        cmbProductCategory.setModel(myModel);
     }
 
     /**
@@ -50,7 +50,7 @@ public class ProductEditorDialog extends javax.swing.JDialog {
         txtProductDescription = new javax.swing.JTextArea();
         lblProductDescription = new javax.swing.JLabel();
         lblProductCategory = new javax.swing.JLabel();
-        cbProductCategory = new javax.swing.JComboBox<>();
+        cmbProductCategory = new javax.swing.JComboBox<>();
         lblProductPrice = new javax.swing.JLabel();
         txtProductPrice = new javax.swing.JTextField();
         lblQuantityInStock = new javax.swing.JLabel();
@@ -72,7 +72,7 @@ public class ProductEditorDialog extends javax.swing.JDialog {
 
         lblProductCategory.setText("Category:");
 
-        cbProductCategory.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cmbProductCategory.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         lblProductPrice.setText("Price:");
 
@@ -115,7 +115,7 @@ public class ProductEditorDialog extends javax.swing.JDialog {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jScrollPane1)
-                            .addComponent(cbProductCategory, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addComponent(cmbProductCategory, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(23, 23, 23)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -149,7 +149,7 @@ public class ProductEditorDialog extends javax.swing.JDialog {
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cbProductCategory, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cmbProductCategory, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblProductCategory))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -174,7 +174,7 @@ public class ProductEditorDialog extends javax.swing.JDialog {
         String productId = txtProductId.getText();
         String productName = txtProductName.getText();
         String productDescription = txtProductDescription.getText();
-        String productCategory = cbProductCategory.getSelectedItem().toString();
+        String productCategory = cmbProductCategory.getSelectedItem().toString();
         BigDecimal productPrice = new BigDecimal(txtProductPrice.getText());
         BigDecimal productQuantityInStock = new BigDecimal(txtProductQuantityInStock.getText());
         
@@ -247,7 +247,7 @@ public class ProductEditorDialog extends javax.swing.JDialog {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnProductCancel;
     private javax.swing.JButton btnProductSave;
-    private javax.swing.JComboBox<String> cbProductCategory;
+    private javax.swing.JComboBox<String> cmbProductCategory;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblProductCategory;
     private javax.swing.JLabel lblProductDescription;
