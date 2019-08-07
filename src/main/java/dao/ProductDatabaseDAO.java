b3c5be01c2012adb37f79243c450af17539f4204
rofth173 <rofth173@SOB0484L.staff.uod.otago.ac.nz>
2019-08-07 17:01:32 +1200

    Implemented getProducts method
    References #11

--- src/main/java/dao/ProductDatabaseDAO.java
+++ src/main/java/dao/ProductDatabaseDAO.java
@@ -6,7 +6,12 @@
 package dao;
 
 import domain.Product;
+import java.math.BigDecimal;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import dao.DbConnection;
 
 /**
  *
@@ -14,7 +19,7 @@ import java.util.Collection;
  */
 public class ProductDatabaseDAO implements DAO {
     
-    private static String URI;
+    private static String URI = DbConnection.getDefaultConnectionUri();
     
     public ProductDatabaseDAO() {}
     
@@ -39,7 +44,44 @@ public class ProductDatabaseDAO implements DAO {
 
     @Override
     public Collection<Product> getProducts() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "select * from Product order by productId";
+        
+        try {
+            // get a connection to the database
+            Connection dbCon = DbConnection.getConnection(URI);
+            
+            // create the statement
+            PreparedStatement stmt = dbCon.prepareStatement(sql);
+            
+            // execute the query
+            ResultSet rs = stmt.executeQuery();
+
+            // Using a List to preserve the order in which the data was returned from the query.
+            List<Product> products = new ArrayList<>();
+
+            // iterate through the query results
+            while (rs.next()) {
+
+                // get the data out of the query
+                String productId = rs.getString("ProductId");
+                String name = rs.getString("name");
+                String description = rs.getString("description");
+                String category = rs.getString("category");
+                BigDecimal listprice = rs.getBigDecimal("listprice");
+                BigDecimal quantityInStock = rs.getBigDecimal("quantityInStock");
+
+                // use the data to create a product object
+                Product p = new Product(productId, name, description, category, listprice, quantityInStock);
+
+                // and put it in the collection
+                products.add(p);
+            }
+            
+            return products;
+            
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 
     @Override
