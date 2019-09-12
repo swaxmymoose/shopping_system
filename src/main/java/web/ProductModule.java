4f8b0c68c2cc5d98650796b147e5393bc09cb7dc
thomasroff <thomasroff@thomass-mbp.lan>
2019-09-12 12:14:54 +1200

    Fixed typo

--- src/main/java/web/ProductModule.java
+++ src/main/java/web/ProductModule.java
@@ -24,7 +24,7 @@ public class ProductModule extends Jooby{
         get("/api/categories", () -> dao.getCategories());
         get("/api/categories/:cat", (req) -> {
             String cat = req.param("cat").value();
-            return dao.getProductById(cat);
+            return dao.getProductsByCategory(cat);
         });
         
     }
