c18b10bd90697268b214783a28b6aafb0766f0dd
thomasroff <thomasroff@thomass-mbp.lan>
2019-09-16 13:32:13 +1200

    Created sign_in page
    References #20

--- public/js/shopping.js
+++ public/js/shopping.js
@@ -24,22 +24,46 @@ module.controller('ProductController',function(productDAO, categoryDAO) {
     };
 });
 
-module.controller('CustomerController',function(registerDAO) {
+module.controller('CustomerController',function(registerDAO, signInDAO, $sessionStorage, $window) {
+    this.signInMessage ="Please sign in to continue.";
+    
     this.registerCustomer = function(customer) {
         registerDAO.save(null, customer,
         //success callback
         function() {
-            $window.location ='signin.html';
+            $window.location ='sign_in.html';
         },
         //error callback
         function(error) {
             console.log(error);
         });
     };
+    
+    //alias 'this' so that we can access it inside callback functions
+    let ctrl = this;
+    this.signIn = function(username, password) {
+        //get customer from web service
+        signInDAO.get({'username': username},
+        //success
+        function(customer) {
+            //also store the retrieved customer
+            $sessionStorage.customer = customer;
+            //redirect to home
+            $window.location.href ='.';
+        },
+        //fail
+        function() {
+            ctrl.signInMessage ='Sign in failed. Please try again.';
+        });
+    };
 });
 
 module.factory('registerDAO',function($resource) {
     return $resource('/api/register');
+});
+
+module.factory('signInDAO',function($resource) {
+    return $resource('/api/customers/:username');
 });
 
 
